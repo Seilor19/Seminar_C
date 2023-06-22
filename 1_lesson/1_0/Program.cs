836537c4fb6419b6e0829df847d23f8e22ce2194
Илья <seilormoonn@gmail.com>
2023-06-22 22:02:02 +0400

    new primer

---
+++ 1_lesson/1_0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+//string num_str = Console.ReadLine()!;
+//int num = int.Parse(num_str);
+int num = int.Parse(Console.ReadLine()); //считывание данных(краткая версия)
+Console.WriteLine(num * num);
+
