9414849824c83c688d22a624be6328627cd1b230
Илья <seilormoonn@gmail.com>
2023-06-22 23:16:44 +0400

    1_3

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int count = -a;
+    Console.WriteLine(count);
+while (count!=a){
+        if (a>0)
+        count++;
+        else
+        count--;
+    
+    Console.WriteLine(count);
+    }
