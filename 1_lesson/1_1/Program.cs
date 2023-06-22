6fb4ad953240f9ce9f485a489f1603e3c45a5459
Илья <seilormoonn@gmail.com>
2023-06-22 22:29:47 +0400

    1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+if (a/b  == b)
+{
+    Console.Write("Yes");
+}
+else {
+    Console.Write("No");
+}
