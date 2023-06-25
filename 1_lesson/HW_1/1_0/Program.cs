22e75cbb58be6ad5d1bba644f70e4a3d38c62135
Илья <seilormoonn@gmail.com>
2023-06-26 00:00:22 +0400

    Второе задание

---
+++ 1_lesson/HW_1/1_0/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите 1ое число");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 2ое число");
+
+int b = int.Parse(Console.ReadLine()!);
+int max;
+int min;
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
---
+++ 1_lesson/HW_1/HW_1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1ое число");
+
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 2ое число");
+
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите 3ое число");
+
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if(b>max) max = b;
+if(c>max) max = c ;  
+
+ 
+
+Console.WriteLine("Максимальное значение = ");
+Console.WriteLine(max);
