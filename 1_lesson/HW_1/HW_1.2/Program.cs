35e5c6b5591d53ec0e4bddd5667e5407d135a6f9
Илья <seilormoonn@gmail.com>
2023-06-26 00:21:53 +0400

    Третье задание

---
+++ 1_lesson/HW_1/HW_1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите число");
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 2 == 0) {
+    Console.WriteLine("четное");
+}
+    else { 
+        Console.WriteLine("не четное");
+            }
