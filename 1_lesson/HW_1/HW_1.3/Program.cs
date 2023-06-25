0a1056f127e649b4642cd8fa536365e3451791c3
Илья <seilormoonn@gmail.com>
2023-06-26 00:49:00 +0400

    HW_1.3

---
+++ 1_lesson/HW_1/HW_1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+
+int N = int.Parse(Console.ReadLine()!);
+int i =1 ;
+
+do
+{
+    if (i % 2 == 0) 
+    {
+    Console.WriteLine(i);
+    i=i+1;
+    }
+    else i=i+1;
+}
+while (i<=N);
+
+    
