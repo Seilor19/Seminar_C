d27c7960728ce8a0119fe30485a50ece9ce4b28b
Илья <seilormoonn@gmail.com>
2023-06-26 23:49:37 +0400

    2_1

---
+++ 2_lesson/1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿void Crat(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else 
+    {
+        Console.WriteLine("Нет");
+    }
+
+}
+
+int numA = int.Parse(Console.ReadLine()!);
+Crat(numA);
---
+++ 2_lesson/HW_2/2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+
+int number = int.Parse(Console.ReadLine()!);
+if (number >= 100 && number < 1000)
+{
+int mid = ((number % 100 - number % 10) / 10);
+Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+}
+else
+Console.WriteLine("Это число не трехзначное");
