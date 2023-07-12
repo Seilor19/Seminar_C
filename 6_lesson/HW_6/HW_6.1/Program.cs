f733a43a4e6132d0e7b10cac2b84d137cf34bbcd
Илья <seilormoonn@gmail.com>
2023-07-13 00:42:12 +0400

    hw_6

---
+++ 6_lesson/HW_6/HW_6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.Если пользователь вводит 0 цикл прерывается
+
+Console.Write("Сколько чисел?");
+int M = int.Parse(Console.ReadLine()!);
+int res=0;
+
+for (int i = 1; i <= M; i++)
+{
+    Console.WriteLine($"Введите {i} число ");
+    int a = int.Parse(Console.ReadLine()!);
+  
+    if (a==0)
+    {           
+                Console.WriteLine("Введеное число равно 0");
+                break;
+    } 
+    
+    else if (a>0) res=res+1;
+}          
+    
+
+
+Console.WriteLine($"Количество положительных чисел = {res} ");
---
+++ 6_lesson/HW_6/HW_6.2/Program.cs
@@ -0,0 +1,65 @@
+﻿//Напишите программу, которая будет создавать
+//копию заданного массива с помощью поэлементного
+//копирования. 
+
+Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[row, col];
+int[,] copy = new int[row, col];
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+
+    }
+}
+
+void CopyArray(int[,] copy)
+{   for (int i = 0; i <= row-1; i++)
+    {
+        for (int j = 0; j < col; j++)
+            {
+            copy[i,j] = matrix[i,j];
+            }
+    }
+}
+
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintCopy(int[,] copy)
+{
+    for (int i = 0; i < copy.GetLength(0); i++)
+    {
+        for (int j = 0; j < copy.GetLength(1); j++)
+        {
+            Console.Write($"{copy[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+CopyArray(copy);
+PrintCopy(copy);
