7390ffc2b3105660c74a705060bdd63403541e35
Илья <seilormoonn@gmail.com>
2023-07-17 21:13:47 +0400

    7 hw

--- 7_lesson/HW_7/HW_7.1/Program.cs
+++ 7_lesson/HW_7/HW_7.1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+double[,] matrix = new double[row, col];
+
+Console.Write("Введите минимальное значение элемента массива : ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int max = int.Parse(Console.ReadLine()!);
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble(), 1);
+        }
+
+    }
+}
+FillArray(matrix);
+PrintArray(matrix);
--- 7_lesson/HW_7/HW_7.2/Program.cs
+++ 7_lesson/HW_7/HW_7.2/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[row, col];
+
+Console.Write("Введите минимальное значение элемента массива : ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int max = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Строку : ");
+int IRow = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Столбец : ");
+int JCol = int.Parse(Console.ReadLine()!);
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+void Find(int[,] matrix)
+{
+    if (row<IRow || col<JCol)
+    Console.Write("Вне диапазона адресов");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == matrix[IRow,JCol]) 
+            {
+                Console.Write($"Значение элемента {matrix[i,j]}");
+            
+            }
+            
+        }
+    
+    }
+    
+ }
+    
+
+
+FillArray(matrix);
+PrintArray(matrix);
+Find(matrix);
