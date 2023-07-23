bb8ac6ee85b0cfa5df33453b0f6aeddb75722118
Илья <seilormoonn@gmail.com>
2023-07-24 00:15:33 +0400

    HW_8_9

--- 8_lesson/HW_8/HW_8.3/Program.cs
+++ 8_lesson/HW_8/HW_8.3/Program.cs
@@ -3,14 +3,25 @@ int row = int.Parse(Console.ReadLine()!);
 Console.Write("Кол-во столбцов 1ой матрицы ");
 int col = int.Parse(Console.ReadLine()!);
 int[,] matrix = new int[row, col];
+
+Console.Write("Кол-во строк 2 ой матрицы ");
+int Srow = int.Parse(Console.ReadLine()!);
 Console.Write("Кол-во столбцов 2 ой матрицы ");
 int Scol = int.Parse(Console.ReadLine()!);
-int[,] Smatrix = new int[row, Scol];
+int[,] Smatrix = new int[Srow, Scol];
 
 Console.Write("Введите минимальное значение элемента матрицы : ");
 int min = int.Parse(Console.ReadLine()!);
 Console.Write("Введите максимальное значение элемента матрицы : ");
 int max = int.Parse(Console.ReadLine()!);
+
+if (col != Srow)
+{
+  Console.WriteLine("Такие матрицы умножать нельзя!");
+  return;
+}
+
+
 
 void PrintArray(int[,] matrix)
 {
@@ -35,29 +46,39 @@ void FillArray(int[,] matrix)
     }
 }
 
-int[,] ResMatrix = new int[row,Scol];
 
-int MultiMatrix(int[,] matrix, int[,] Smatrix, int[,] ResMatrix)
+
+int[,] MultiMatrix(int[,] matrix, int[,] Smatrix)
 {
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < col; j++)
+   
+    int[,] arrayC = new int[matrix.GetLength(0), Smatrix.GetLength(1)];
+ 
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        for (int j = 0; j < Smatrix.GetLength(1); j++)
         {
-            int sum = 0;
-            for (int k = 0; k < col; k++)
-                {
-                    sum += matrix[i,k] * Smatrix[k,j];
-                }
-            ResMatrix[i,j] = sum;
+          for (int k = 0; k < matrix.GetLength(1); k++)
+          {
+            arrayC[i, j] += matrix[i, k] * Smatrix[k, j];
+          }
         }
-  }
+      }
+      return arrayC;
+ }
   
-}
+    
+   
+
+
 FillArray(matrix);
 PrintArray(matrix);
-FillArray(Smatrix);
+
 Console.WriteLine();
+
+FillArray(Smatrix);
 PrintArray(Smatrix);
 
-MultiMatrix(matrix, Smatrix, ResMatrix);
-PrintArray(ResMatrix);
+Console.WriteLine();
+
+MultiMatrix(matrix, Smatrix);
+PrintArray(MultiMatrix(matrix, Smatrix));
--- 8_lesson/HW_8/HW_8.4/Program.cs
+++ 8_lesson/HW_8/HW_8.4/Program.cs
@@ -9,3 +9,78 @@ int min = int.Parse(Console.ReadLine()!);
 Console.Write("Введите максимальное значение элемента массива : ");
 int max = int.Parse(Console.ReadLine()!);
 
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+int[,] FindPositionSmallElement(int[,] matrix, int[,] position)
+{
+  int temp = matrix[0, 0];
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (matrix[i, j] <= temp)
+      {
+        position[0, 0] = i;
+        position[0, 1] = j;
+        temp = matrix[i, j];
+      }
+    }
+  }
+  Console.WriteLine($"\nMинимальный элемент: {temp}");
+  return position;
+}
+
+void DeleteLines(int[,] matrix, int[,] positionSmallElement, int[,] arrayWithoutLines)
+{
+  int k = 0, l = 0;
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
+      {
+        arrayWithoutLines[k, l] = matrix[i, j];
+        l++;
+      }
+    }
+    l = 0;
+    if (positionSmallElement[0, 0] != i)
+    {
+      k++;
+    }
+  }
+}
+FillArray(matrix);
+PrintArray(matrix);
+
+int[,] positionSmallElement = new int[1, 2];
+positionSmallElement = FindPositionSmallElement(matrix, positionSmallElement);
+Console.Write($"Позиция элемента: \n");
+PrintArray(positionSmallElement);
+
+int[,] arrayWithoutLines = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+DeleteLines(matrix, positionSmallElement, arrayWithoutLines);
+Console.WriteLine($"\nПолучившийся массив:");
+PrintArray(arrayWithoutLines);
+
